cb4b564734e42e8a2ec514e1cf77c0c4de7239cf
Allyson Camp <Allysoncamp92@gmail.com>
2023-01-10 17:04:54 -0800

    added spans and state, console logged and working currently

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* Imports */
 
 /* Get DOM Elements */
+
 //buttons
 const orangeShellButton = document.getElementById('orange-shell-button');
 const blueShellButton = document.getElementById('blue-shell-button');
@@ -12,9 +13,19 @@ const orangeShellElement = document.getElementById('orange-shell-element');
 const blueShellElement = document.getElementById('blue-shell-element');
 const pinkShellElement = document.getElementById('pink-shell-element');
 
+//spans
+const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
+const totalEl = document.getElementById('total');
+
 /* State */
+let correctGuesses = 0;
+let incorrectGuesses = 0;
 
 /* Events */
+
+    /*click events*/
+
 
 /* Display Functions */
 
